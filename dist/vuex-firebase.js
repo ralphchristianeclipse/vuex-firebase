6c2a2f9fc3018044b70f2fc6d35f1490cedb034f
Ralph Christian Eclipse <ralphchristianeclipse@gmail.com>
2017-03-14 14:41:26 +0000

    Fixed VUEX_FIREBASE_SAVE

--- dist/vuex-firebase.js
+++ dist/vuex-firebase.js
@@ -125,8 +125,8 @@ var FirebaseBind = function () {
         value: function record(snap) {
             return _extends({
                 _: {
-                    _key: snap.key,
-                    _ref: this.ref
+                    key: snap.key,
+                    ref: this.ref
                 }
             }, snap.val());
         }
@@ -252,179 +252,177 @@ exports.default = FirebaseBind;
 
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+	value: true
 });
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
 exports.default = function (store, fb) {
-    var Vue = store._watcherVM || store._vm;
-    var state = {
-        auth: fb.auth(),
-        database: fb.database(),
-        storage: fb.storage(),
-        firebase: {}
-    };
+	var Vue = store._watcherVM || store._vm;
+	var state = {
+		auth: fb.auth(),
+		database: fb.database(),
+		storage: fb.storage(),
+		firebase: {}
+	};
 
-    var mutations = {
-        //Mutation for setting the firebaseBind object
-        //Using Vue.set to make it reactive
-        VUEX_FIREBASE_BINDED: function VUEX_FIREBASE_BINDED(state, payload) {
-            if (state.firebase[payload.ref]) return;
-            Vue.$set(state.firebase, payload.ref, payload);
-        },
+	var mutations = {
+		//Mutation for setting the firebaseBind object
+		//Using Vue.set to make it reactive
+		VUEX_FIREBASE_BINDED: function VUEX_FIREBASE_BINDED(state, payload) {
+			if (state.firebase[payload.ref]) return;
+			Vue.$set(state.firebase, payload.ref, payload);
+		},
 
-        //Unbind the firebaseBind object
-        VUEX_FIREBASE_UNBINDED: function VUEX_FIREBASE_UNBINDED(state, payload) {
-            Vue.$delete(state.firebase, payload.ref);
-        },
-        VUEX_FIREBASE_ADDED: function VUEX_FIREBASE_ADDED(state, _ref) {
-            var index = _ref.index,
-                record = _ref.record;
+		//Unbind the firebaseBind object
+		VUEX_FIREBASE_UNBINDED: function VUEX_FIREBASE_UNBINDED(state, payload) {
+			Vue.$delete(state.firebase, payload.ref);
+		},
+		VUEX_FIREBASE_ADDED: function VUEX_FIREBASE_ADDED(state, _ref) {
+			var index = _ref.index,
+			    record = _ref.record;
 
-            state.firebase[record._.ref].data.splice(index, 0, record);
-        },
-        VUEX_FIREBASE_CHANGED: function VUEX_FIREBASE_CHANGED(state, _ref2) {
-            var index = _ref2.index,
-                record = _ref2.record;
+			state.firebase[record._.ref].data.splice(index, 0, record);
+		},
+		VUEX_FIREBASE_CHANGED: function VUEX_FIREBASE_CHANGED(state, _ref2) {
+			var index = _ref2.index,
+			    record = _ref2.record;
 
-            state.firebase[record._.ref].data.splice(index, 1, record);
-        },
-        VUEX_FIREBASE_REMOVED: function VUEX_FIREBASE_REMOVED(state, _ref3) {
-            var index = _ref3.index,
-                record = _ref3.record;
+			state.firebase[record._.ref].data.splice(index, 1, record);
+		},
+		VUEX_FIREBASE_REMOVED: function VUEX_FIREBASE_REMOVED(state, _ref3) {
+			var index = _ref3.index,
+			    record = _ref3.record;
 
-            state.firebase[record._.ref].data.splice(index, 1);
-        },
-        VUEX_FIREBASE_MOVED: function VUEX_FIREBASE_MOVED(state, _ref4) {
-            var index = _ref4.index,
-                record = _ref4.record,
-                newIndex = _ref4.newIndex;
+			state.firebase[record._.ref].data.splice(index, 1);
+		},
+		VUEX_FIREBASE_MOVED: function VUEX_FIREBASE_MOVED(state, _ref4) {
+			var index = _ref4.index,
+			    record = _ref4.record,
+			    newIndex = _ref4.newIndex;
 
-            var array = state.firebase[record._.ref].data;
-            array.splice(newIndex, 0, state.firebase[record._.ref].data.splice(index, 1)[0]);
-        }
-    };
+			var array = state.firebase[record._.ref].data;
+			array.splice(newIndex, 0, state.firebase[record._.ref].data.splice(index, 1)[0]);
+		}
+	};
 
-    var getters = {
-        // Auth object of firebase
-        $auth: function $auth(state) {
-            return state.auth;
-        },
+	var getters = {
+		// Auth object of firebase
+		$auth: function $auth(state) {
+			return state.auth;
+		},
 
-        // Get the FirebaseBind Object by passing its key or source
-        $firebase: function $firebase(state) {
-            return function (key) {
-                return state.firebase[key];
-            };
-        },
+		// Get the FirebaseBind Object by passing its key or source
+		$firebase: function $firebase(state) {
+			return function (key) {
+				return state.firebase[key];
+			};
+		},
 
-        //Firebase timestamp
-        $timestamp: function $timestamp(state) {
-            return fb.database.ServerValue.TIMESTAMP;
-        },
+		//Firebase timestamp
+		$timestamp: function $timestamp(state) {
+			return fb.database.ServerValue.TIMESTAMP;
+		},
 
-        //Getter for firebase.database()
-        $database: function $database(state) {
-            return state.database;
-        },
+		//Getter for firebase.database()
+		$database: function $database(state) {
+			return state.database;
+		},
 
-        //firebase.storage()
-        $storage: function $storage(state) {
-            return state.storage;
-        }
-    };
+		//firebase.storage()
+		$storage: function $storage(state) {
+			return state.storage;
+		}
+	};
 
-    var actions = {
-        /*
-            _ref = the target node ex. 'users'
-            _key = the key for the data to send or you can omit it to use the push key
-            _hook = used for chaining key actions based on the key value good for updating relations of nodes
-            _time = set to true if you want to have created and updated time stamps
-        */
-        VUEX_FIREBASE_SAVE: function VUEX_FIREBASE_SAVE(_ref5, payload) {
-            var _this = this;
+	var actions = {
+		/*
+   _.ref = the target node ex. 'users'
+   _.key = the key for the data to send or you can omit it to use the push key
+   _.hook = used for chaining key actions based on the key value good for updating relations of nodes
+   _.time = set to true if you want to have created and updated time stamps
+   */
+		VUEX_FIREBASE_SAVE: function VUEX_FIREBASE_SAVE(_ref5, payload) {
+			var _this = this;
 
-            var getters = _ref5.getters,
-                commit = _ref5.commit;
-            return _asyncToGenerator(regeneratorRuntime.mark(function _callee() {
-                var _payload, _, data, firebase;
+			var getters = _ref5.getters;
+			return _asyncToGenerator(regeneratorRuntime.mark(function _callee() {
+				var _payload, _, data, _ref6, key, ref, hook, time, firebase;
 
-                return regeneratorRuntime.wrap(function _callee$(_context) {
-                    while (1) {
-                        switch (_context.prev = _context.next) {
-                            case 0:
-                                _payload = _extends({}, payload), _ = _payload._, data = _objectWithoutProperties(_payload, ['_']);
+				return regeneratorRuntime.wrap(function _callee$(_context) {
+					while (1) {
+						switch (_context.prev = _context.next) {
+							case 0:
+								_payload = _extends({}, payload), _ = _payload._, data = _objectWithoutProperties(_payload, ['_']), _ref6 = _ || data, key = _ref6.key, ref = _ref6.ref, hook = _ref6.hook, time = _ref6.time;
 
-
-                                _.key = _.key || getters.$database.ref(_.ref).push().key;
-                                firebase = getters.$database.ref(_.ref).child(_.key);
+								key = key || getters.$database.ref(ref).push().key;
+								firebase = getters.$database.ref(ref).child(key);
 
 
-                                if (_.hook && _.key) {
-                                    _.hook(_.key);
-                                }
+								if (hook && key) {
+									hook(key);
+								}
 
-                                if (Object.keys(data).length) {
-                                    _context.next = 8;
-                                    break;
-                                }
+								if (!(!Object.keys(data).length || !_)) {
+									_context.next = 8;
+									break;
+								}
 
-                                _context.next = 7;
-                                return firebase.remove();
+								_context.next = 7;
+								return firebase.remove();
 
-                            case 7:
-                                return _context.abrupt('return');
+							case 7:
+								return _context.abrupt('return');
 
-                            case 8:
+							case 8:
 
-                                if (!_.time) {
-                                    data.created = data.created || getters.$timestamp;
-                                    data.updated = getters.$timestamp;
-                                }
+								if (!time) {
+									data.created = data.created || getters.$timestamp;
+									data.updated = getters.$timestamp;
+								}
 
-                                _context.next = 11;
-                                return firebase.update(data);
+								_context.next = 11;
+								return firebase.update(data);
 
-                            case 11:
-                            case 'end':
-                                return _context.stop();
-                        }
-                    }
-                }, _callee, _this);
-            }))();
-        },
+							case 11:
+							case 'end':
+								return _context.stop();
+						}
+					}
+				}, _callee, _this);
+			}))();
+		},
 
-        //Create new FirebaseBind Objects based on keys
-        VUEX_FIREBASE_BIND: function VUEX_FIREBASE_BIND(_ref6, payload) {
-            var commit = _ref6.commit;
+		//Create new FirebaseBind Objects based on keys
+		VUEX_FIREBASE_BIND: function VUEX_FIREBASE_BIND(_ref7, payload) {
+			var commit = _ref7.commit;
 
-            Object.keys(payload).forEach(function (load) {
-                commit('VUEX_FIREBASE_BINDED', new _bind2.default(fb.database().ref(load), payload[load], load));
-            });
-        },
+			Object.keys(payload).forEach(function (load) {
+				commit('VUEX_FIREBASE_BINDED', new _bind2.default(fb.database().ref(load), payload[load], load));
+			});
+		},
 
-        //Unbind based on the pass key
-        VUEX_FIREBASE_UNBIND: function VUEX_FIREBASE_UNBIND(_ref7, payload) {
-            var commit = _ref7.commit,
-                getters = _ref7.getters;
+		//Unbind based on the pass key
+		VUEX_FIREBASE_UNBIND: function VUEX_FIREBASE_UNBIND(_ref8, payload) {
+			var commit = _ref8.commit,
+			    getters = _ref8.getters;
 
-            Object.keys(payload).forEach(function (load) {
-                commit('VUEX_FIREBASE_UNBINDED', getters.$firebase(load));
-            });
-        }
-    };
+			Object.keys(payload).forEach(function (load) {
+				commit('VUEX_FIREBASE_UNBINDED', getters.$firebase(load));
+			});
+		}
+	};
 
-    var VuexFirebase = {
-        state: state,
-        getters: getters,
-        mutations: mutations,
-        actions: actions
-    };
+	var VuexFirebase = {
+		state: state,
+		getters: getters,
+		mutations: mutations,
+		actions: actions
+	};
 
-    store.registerModule('VuexFirebase', VuexFirebase);
-    //Set the static store for FirebaseBind class
-    _bind2.default.store = store;
+	store.registerModule('VuexFirebase', VuexFirebase);
+	//Set the static store for FirebaseBind class
+	_bind2.default.store = store;
 };
 
 var _bind = __webpack_require__(0);
--- src/index.js
+++ src/index.js
@@ -1,117 +1,117 @@
 import FirebaseBind from './bind';
-export default function(store, fb) {
-    const Vue = store._watcherVM || store._vm;
-    const state = {
-        auth: fb.auth(),
-        database: fb.database(),
-        storage: fb.storage(),
-        firebase: {},
-    };
+export default function (store, fb) {
+	const Vue = store._watcherVM || store._vm;
+	const state = {
+		auth: fb.auth(),
+		database: fb.database(),
+		storage: fb.storage(),
+		firebase: {},
+	};
 
-    const mutations = {
-        //Mutation for setting the firebaseBind object
-        //Using Vue.set to make it reactive
-        VUEX_FIREBASE_BINDED(state,payload) {
-            if(state.firebase[payload.ref]) return;
-            Vue.$set(state.firebase,payload.ref,payload);  
-        },
-        //Unbind the firebaseBind object
-        VUEX_FIREBASE_UNBINDED(state,payload) {
-            Vue.$delete(state.firebase,payload.ref);  
-        },
-        
-        VUEX_FIREBASE_ADDED(state, { index, record }) {
-            state.firebase[record._.ref].data.splice(index, 0, record);
-        },
-        VUEX_FIREBASE_CHANGED(state, { index, record }) {
-            state.firebase[record._.ref].data.splice(index, 1, record);
-        },
-        VUEX_FIREBASE_REMOVED(state, { index, record }) {
-            state.firebase[record._.ref].data.splice(index, 1);
-        },
-        VUEX_FIREBASE_MOVED(state, { index, record, newIndex }) {
-            let array = state.firebase[record._.ref].data;
-            array.splice(newIndex, 0, state.firebase[record._.ref].data.splice(index, 1)[0]);
-        },
-    };
+	const mutations = {
+		//Mutation for setting the firebaseBind object
+		//Using Vue.set to make it reactive
+		VUEX_FIREBASE_BINDED(state, payload) {
+			if (state.firebase[payload.ref]) return;
+			Vue.$set(state.firebase, payload.ref, payload);
+		},
+		//Unbind the firebaseBind object
+		VUEX_FIREBASE_UNBINDED(state, payload) {
+			Vue.$delete(state.firebase, payload.ref);
+		},
 
-    const getters = {
-        // Auth object of firebase
-        $auth(state) {
-            return state.auth;
-        },
-        // Get the FirebaseBind Object by passing its key or source
-        $firebase(state) {
-            return key => state.firebase[key];
-        },
-        //Firebase timestamp
-        $timestamp(state) {
-            return fb.database.ServerValue.TIMESTAMP;
-        },
-        //Getter for firebase.database()
-        $database(state) {
-            return state.database;
-        },
-        //firebase.storage()
-        $storage(state) {
-            return state.storage;
-        },
-    };
+		VUEX_FIREBASE_ADDED(state, {index, record}) {
+			state.firebase[record._.ref].data.splice(index, 0, record);
+		},
+		VUEX_FIREBASE_CHANGED(state, {index, record}) {
+			state.firebase[record._.ref].data.splice(index, 1, record);
+		},
+		VUEX_FIREBASE_REMOVED(state, {index, record}) {
+			state.firebase[record._.ref].data.splice(index, 1);
+		},
+		VUEX_FIREBASE_MOVED(state, {index, record, newIndex}) {
+			let array = state.firebase[record._.ref].data;
+			array.splice(newIndex, 0, state.firebase[record._.ref].data.splice(index, 1)[0]);
+		},
+	};
 
-    const actions = {
-        /*
-            _ref = the target node ex. 'users'
-            _key = the key for the data to send or you can omit it to use the push key
-            _hook = used for chaining key actions based on the key value good for updating relations of nodes
-            _time = set to true if you want to have created and updated time stamps
-        */
-        async VUEX_FIREBASE_SAVE({ getters, commit }, payload) {
-            let { _, ...data } = {...payload};
-            
-                _.key = _.key || getters.$database.ref(_.ref).push().key;
-                let firebase = getters.$database.ref(_.ref).child(_.key);
-                
-                if(_.hook && _.key) {
-                   _.hook(_.key);
-                }
-                
-                if(!Object.keys(data).length) {
-                    await firebase.remove();
-                    return;
-                }
-                
-                if(!_.time) {
-                    data.created = data.created || getters.$timestamp;
-                    data.updated = getters.$timestamp;
-                }
-                
-                await firebase.update(data);
+	const getters = {
+		// Auth object of firebase
+		$auth(state) {
+			return state.auth;
+		},
+		// Get the FirebaseBind Object by passing its key or source
+		$firebase(state) {
+			return key => state.firebase[key];
+		},
+		//Firebase timestamp
+		$timestamp(state) {
+			return fb.database.ServerValue.TIMESTAMP;
+		},
+		//Getter for firebase.database()
+		$database(state) {
+			return state.database;
+		},
+		//firebase.storage()
+		$storage(state) {
+			return state.storage;
+		},
+	};
 
-        },
-        //Create new FirebaseBind Objects based on keys
-        VUEX_FIREBASE_BIND({commit},payload) {
-            Object.keys(payload).forEach(load => {
-                commit('VUEX_FIREBASE_BINDED',
-                new FirebaseBind(fb.database().ref(load),payload[load],load));                
-            })
-        },
-        //Unbind based on the pass key
-        VUEX_FIREBASE_UNBIND({commit,getters},payload) {
-            Object.keys(payload).forEach(load => {
-                commit('VUEX_FIREBASE_UNBINDED',getters.$firebase(load));
-            });
-        }
-    };
-    
-    let VuexFirebase = {
-        state,
-        getters,
-        mutations,
-        actions
-    };
-    
-    store.registerModule('VuexFirebase', VuexFirebase);
-    //Set the static store for FirebaseBind class
-    FirebaseBind.store = store;
-    
+	const actions = {
+		/*
+		 _.ref = the target node ex. 'users'
+		 _.key = the key for the data to send or you can omit it to use the push key
+		 _.hook = used for chaining key actions based on the key value good for updating relations of nodes
+		 _.time = set to true if you want to have created and updated time stamps
+		 */
+		async VUEX_FIREBASE_SAVE({getters}, payload) {
+			let {_,...data} = {...payload},
+				{key,ref,hook,time} = _ || data;
+			key = key || getters.$database.ref(ref).push().key;
+			let firebase = getters.$database.ref(ref).child(key);
+
+			if (hook && key) {
+				hook(key);
+			}
+
+			if (!Object.keys(data).length || !_) {
+				await firebase.remove();
+				return;
+			}
+
+			if (!time) {
+				data.created = data.created || getters.$timestamp;
+				data.updated = getters.$timestamp;
+			}
+
+			await firebase.update(data);
+
+		},
+		//Create new FirebaseBind Objects based on keys
+		VUEX_FIREBASE_BIND({commit}, payload) {
+			Object.keys(payload).forEach(load => {
+				commit('VUEX_FIREBASE_BINDED',
+					new FirebaseBind(fb.database().ref(load), payload[load], load));
+			})
+		},
+		//Unbind based on the pass key
+		VUEX_FIREBASE_UNBIND({commit, getters}, payload) {
+			Object.keys(payload).forEach(load => {
+				commit('VUEX_FIREBASE_UNBINDED', getters.$firebase(load));
+			});
+		}
+	};
+
+	let VuexFirebase = {
+		state,
+		getters,
+		mutations,
+		actions
+	};
+
+	store.registerModule('VuexFirebase', VuexFirebase);
+	//Set the static store for FirebaseBind class
+	FirebaseBind.store = store;
+
 }
